a657f4c794036aebe06c6236bb0b6853f751d84e
Gulf4Romeo <nick.buzali@gmail.com>
2025-12-23 19:48:34 -0500

    Revert to original JPG logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-2">
               <img 
-                src="/Images/rocketrylogo.png" 
+                src="/Images/rocketrylogo.jpg" 
                 alt="UofG Rocketry Club Logo" 
                 className="h-8 object-contain"
               />
